a5bc333f43f77a521a3aeb2b190c07fa1e5eebe7
TheRealski <csavolskis@gmail.com>
2025-06-20 15:40:06 -0700

    Use lief.  Remove elftools and pefile.

--- SymbolStopper.py
+++ SymbolStopper.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import pefile
 import lief
 from pathlib import Path
-import elftools.elf.elffile as elffile
-import elftools.dwarf.dwarfinfo as dwarfinfo
+
+InvalidFileTypes = [".map", ".dmp", ".o", ".obj", ".pdb"]
 
 def ParseArgs():
     parser = argparse.ArgumentParser(prog='SymbolStopper', description='Checks to see if the provided file has debug information.')
@@ -14,15 +13,39 @@ def ParseArgs():
 
     return parser.parse_args()
 
-def DoesPEFileHaveDebugInfo(peFile):
-    if hasattr(peFile, 'DIRECTORY_ENTRY_DEBUG'):
+def DoesPEFileHaveDebugInfo(peFile : lief.PE.Binary | None):
+    if peFile == None:
+        return False
+    
+    if peFile.has_debug:
+        return True
+    
+    diSec = peFile.get_section('.debug_info')
+    if diSec != None and diSec.sizeof_raw_data > 0:
+        return True
+    
+    dSec = peFile.get_section('.debug')
+    if dSec != None and dSec.sizeof_raw_data > 0:
+        return True
+    
+    # If the size of the optional header is 0, then the file is an object file.
+    if peFile.header.sizeof_optional_header == 0:
         return True
 
-def DoesCOFFFileHaveDebugInfo(coffFile):
+    # 0x0200 is IMAGE_FILE_DEBUG_STRIPPED
+    if peFile.header.characteristics & 0x0200 != 0:
+        return True
+    
     return False
 
 def DoesELFFileHaveDebugInfo(elfFile):
-    return True
+    if elfFile.has_debug:
+        return True
+    
+    if any(section.name.startswith('.debug') and section.size > 0 for section in elfFile.sections):
+        return True
+
+    return False
 
 def CrackFile(file, byteCount):
     byteList = []
@@ -32,24 +55,27 @@ def CrackFile(file, byteCount):
         return byteList
 
 def DoesFileHaveDebugInfo(inFile):
-    if ".map" in inFile.name or ".dmp" in inFile.name:
+    if any(inFile.name.endswith(ext) for ext in InvalidFileTypes):
         return True
 
-    pe = pefile.PE(inFile, fast_load=True)
-    if pe.is_exe() or pe.is_dll() or pe.is_driver():
-        return DoesPEFileHaveDebugInfo(pe)
-    
     byteStr = str(CrackFile(inFile, 100))
     if "Microsoft C/C++" in byteStr and ("MSF 7.00" in byteStr or "program database 2.00" in byteStr):
         return True    #Likely a PDB file
     
+    liefFile = lief.parse(inFile)
+    if liefFile == None:
+        return False
+    elif liefFile.format == lief.PE:
+        return DoesPEFileHaveDebugInfo(lief.PE.parse(inFile))
+    elif liefFile.format == lief.ELF:
+        return DoesELFFileHaveDebugInfo(lief.ELF.parse(inFile))
+    elif liefFile.format == lief.dwarf:
+        return True
+    
+    #If we get here, then we need to check if the file is an object file (COFF) but not named as one.
     COFFMagic = int.from_bytes(CrackFile(inFile, 4), 'little')
     if COFFMagic == 332 or COFFMagic == 34404 or COFFMagic == 512:
-        return DoesCOFFFileHaveDebugInfo(inFile)
-    
-    ELFMagic = int.from_bytes(CrackFile(inFile, 8), 'little')
-    if ELFMagic == 1179403647:
-        return DoesELFFileHaveDebugInfo(inFile)
+        return True
     
     return False
 
